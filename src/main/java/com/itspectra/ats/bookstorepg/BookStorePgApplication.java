8e9eb6473830f610779737535d33502af2d89c76
EphraimPhiri <bsc-inf-23-19@unima.ac.mw>
2022-01-18 20:00:20 -0800

    initial setup

---
+++ src/main/java/com/itspectra/ats/bookstorepg/BookStorePgApplication.java
@@ -0,0 +1,13 @@
+package com.itspectra.ats.bookstorepg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookStorePgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookStorePgApplication.class, args);
+	}
+
+}
